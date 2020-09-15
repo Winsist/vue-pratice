a71b4135ca30b94a847f684fd06eb558cc9872cb
wangmy <161049093@qq.com>
2020-09-15 08:31:54 +0800

    a

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    {{num}}
+    <div @click="btn1">button1</div>
+    <div @click="btn2">button2</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+      num:0
+    }
+  },
+  method:{
+    btn1(){
+      this.num++;
+      // console.log(id)
+    },
+    btn2(i){
+      this.num=this.num+2;
+      // console.log(id)
+    },
+  }
+}
+</script>
+
+
