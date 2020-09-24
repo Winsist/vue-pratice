bdfcd22cc46730ca1b6c623fc451a9e9375bdf65
Wininst <1610490093@qq.com>
2020-09-24 18:55:41 +0800

    '事件委托'

--- src/page/base/model.vue
+++ src/page/base/model.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-    model
-    
-  
-
-   
+    <ul @click="learning">
+      <li v-for="(item) of list" :key="item.id" :data-index='item.id'>
+          {{item.num}}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,15 +13,36 @@ export default {
 
   data(){
     return {
+      list:[{
+        num:111,
+        id:1
+      },{
+        num:222,
+        id:2
+      },{
+        num:333,
+        id:3
+      }]
       
     }
   },
   
   
   methods:{
-    // add1(){
-    //   this.num++;
-    // },
+    learning(e){
+      if (e.target.nodeName.toLowerCase() === 'li') {
+            const index = parseInt(e.target.dataset.index)
+            // // 获得引索后，只需要修改data数据就能改变UI了
+            this.doSomething(index)
+            // console.log(e)
+          }
+    },
+    doSomething(index) {
+          // do what you want
+          console.log(index)
+        }
+
+    
   }
 }
 </script>
