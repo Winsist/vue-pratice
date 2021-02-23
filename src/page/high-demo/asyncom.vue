e4fb2c9ee5c1bbdaec3e3951ed4449f9cbe6d74a
wangmy <1610490093@qq.com>
2021-02-23 18:05:28 +0800

    异步组件

--- src/page/high-demo/asyncom.vue
+++ src/page/high-demo/asyncom.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <h1>异步组件</h1>
-    
+    <p>当这个组件很大的时候，异步加载会优化在首页或者用不到的时候的一个性能</p>
+    <Model v-if="flag" />
+    <button @click="flag=true">点击</button>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+      Model: () => import("./model")
+  },
   data() {
-    return {};
+    return {
+      flag: false
+    };
   },
 
   methods: {}
