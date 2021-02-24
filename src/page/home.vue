f27fae8f14f2cd074b647d66c6cde9d5bcfb904f
wangmy <1610490093@qq.com>
2021-02-24 15:37:11 +0800

    高级特性完毕

--- src/page/home.vue
+++ src/page/home.vue
@@ -8,7 +8,7 @@
     <Computed :list="list" @change="handle" />
     <button @click="goDad">去验证兄弟之间的通信</button>
     <button @click="goSelf">去验证自定义事件</button>
-    <button @click="gonextTick">去nextTick</button>
+    <!-- <button @click="gonextTick">去nextTick</button> -->
     <button @click="goVuex">练习VUEX</button>
     <button @click="goThings">练习事件委托</button>
     <button @click="goComputed">学习computed</button>
@@ -51,9 +51,9 @@ export default {
     goSelf() {
       this.$router.push("/todolist/list");
     },
-    gonextTick() {
-      this.$router.push("/high/index");
-    },
+    // gonextTick() {
+    //   this.$router.push("/high/index");
+    // },
     goVuex() {
       this.$router.push("/vuexx/index");
     },
