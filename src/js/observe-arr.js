75928bde40db7e58bd07a994d42841541d1d9feb
wangmy <1610490093@qq.com>
2021-03-01 18:06:34 +0800

    响应式数组

--- src/js/observe-arr.js
+++ src/js/observe-arr.js
@@ -3,6 +3,18 @@ function updatView(){
     console.log('视图更新')
 }
 
+//重新定义数组原型
+const  newArrayPrototype = Array.prototype;
+//创建新对象，原型指向newArrayPrototype，再扩展新的方法不会影响原型
+const newArrayProto = Object.create(newArrayPrototype);
+['push','pop','shift','unshift','sort','splice','reverse'].forEach( methodName =>{
+    newArrayProto[methodName] = function(){
+        updatView();//视图更新
+        newArrayPrototype[methodName].call(this,...arguments)
+    }
+})
+
+
 //重新定义属性，监听起来
 function defineReactive(target,key,value){
     observe(value);//深度监听
@@ -25,6 +37,9 @@ function observe(target){
     if(typeof target !=='object' || target === null){
         return target
     }
+    if(Array.isArray(target)){
+        target.__proto__ = newArrayProto;
+    }
     for(let key in target){
         defineReactive(target,key,target[key])
     }
@@ -37,10 +52,12 @@ const data = {
     info:{
         hobby:'吃鸡',//需要深度监听
     },
-    arr:[1,2,3,4]
+    arr:[1,2,3]
 }
 
 //监听数据
 observe(data)
 
 //测试
+data.arr.push(4);
+// console.log(Array.prototype);
